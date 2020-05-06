56592b37268295a0881fe0ea13c786e6d281d115
NethmiAtapattu <nethmiatapattu97@gmail.com>
2020-05-06 22:00:49 +0530

    intitial changes to app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <p> "Hello Nethmi"</p>
+        <h4>{this.props.title}</h4>
+      </div>
+    );
+  }
+}
+const Body = () => {
+  return <p>This is a body function</p>;
+};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Header title="hello from the app" />
+          <Body />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
