9f6f0c503148ba38cd1d96b1f5aebd9ea111cfee
NethmiAtapattu <nethmiatapattu97@gmail.com>
2020-05-07 07:31:01 +0530

    restored random changes
    deleted the features to test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,9 +23,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <Header title="hello from the app" />
           <Body />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Edit and save to reload.</p>
           <a
             className="App-link"
             href="https://reactjs.org"
